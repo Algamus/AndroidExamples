e71ffaffb02a74438c0f261dd92f3c920f5c4f91
Gökhan Göbüş <gokhangobus@gmail.com>
2017-06-17 19:46:24 +0300

    Add files via upload

---
+++ ExampleOfSampleActivity/main/java/gokhangobus/exampleofsampleactivity/SampleActivity.java
@@ -0,0 +1,15 @@
+package gokhangobus.exampleofsampleactivity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by User on 8.12.2016.
+ */
+public class SampleActivity extends Activity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sample);
+    }
+}
