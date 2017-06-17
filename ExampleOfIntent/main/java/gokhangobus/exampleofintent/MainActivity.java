fb495eb30852a174b5a3b7a384f341ddae11a4bb
Gökhan Göbüş <gokhangobus@gmail.com>
2017-06-17 19:41:00 +0300

    Add files via upload

---
+++ ExampleOfIntent/main/java/gokhangobus/exampleofintent/MainActivity.java
@@ -0,0 +1,66 @@
+package gokhangobus.exampleofintent;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void callIntent(View view){
+        Intent intent = null;
+        switch (view.getId()){
+            case R.id.button01:
+                intent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://www.yeditepe.edu.tr"));
+                startActivity(intent);
+                break;
+            case R.id.button02:
+                //intent = new Intent(Intent.ACTION_CALL, Uri.parse("tel:(+90) 2125780470"));
+                //startActivity(intent);
+                break;
+            case R.id.button03:
+                intent = new Intent(Intent.ACTION_DIAL, Uri.parse("tel: (+49) 12345789"));
+                startActivity(intent);
+                break;
+            case R.id.button04:
+                intent = new Intent(Intent.ACTION_VIEW, Uri.parse("geo:50.123,7.1434?z=19"));
+                startActivity(intent);
+                break;
+            case R.id.button05:
+                intent = new Intent(Intent.ACTION_VIEW, Uri.parse("geo:0,0?q=query"));
+                startActivity(intent);
+                break;
+            case R.id.button06:
+                intent = new Intent("android.media.action.IMAGE_CAPTURE");
+                startActivityForResult(intent,0);
+                break;
+            case R.id.button07:
+                intent = new Intent(Intent.ACTION_VIEW, Uri.parse("content://contacts/people/"));
+                startActivity(intent);
+                break;
+            case R.id.button08:
+                intent = new Intent(Intent.ACTION_EDIT, Uri.parse("content://contacts/people/1"));
+                startActivity(intent);
+                break;
+            default:
+                break;
+
+        }
+    }
+    @Override
+    public void onActivityResult(int requestCode,int resultCode,Intent data){
+        if(resultCode== Activity.RESULT_OK && requestCode ==0){
+            String result = data.toURI();
+            Toast.makeText(this ,result,Toast.LENGTH_LONG);
+        }
+    }
+}
